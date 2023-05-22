0a657fbc345ebc782d5f57fe60e0493e6d38a57e
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-05-22 19:30:15 +0300

    work if def > atk

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+
+
+def sign(x):
+    if x > 0:
+        return 1
+    elif x == 0:
+        return 0
+    elif x < 0:
+        return -1
+    else:
+        raise Exception("Такого не должно быть")
+
+
 atk1 = 31
 def1 = 31
 min_damage1 = 50
@@ -5,10 +18,12 @@ max_damage1 = 50
 hp1 = 220
 
 atk2 = 32
-def2 = 29
+def2 = 36
 min_damage2 = 36
 max_damage2 = 66
 hp2 = 199
 
-damage = min_damage1*(1+0.05*(atk1-def2))
+damage = min_damage1*(1+0.05*abs(atk1-def2))**sign(atk1-def2)
+print(1+0.05*(atk1-def2))
 print(damage)
+print((1+0.05*abs(atk1-def2))**sign(atk1-def2))
