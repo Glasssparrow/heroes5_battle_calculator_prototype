9af7f28d60bbb40d5086283efff969f8f46bac49
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-08-01 19:30:08 +0300

    Update battle.py

--- new_code/battle/battle.py
+++ new_code/battle/battle.py
@@ -9,7 +9,8 @@ def battle(unit1, unit2):
     for x in range(100):
 
         order_list = get_order_list(unit1, unit2)
+        order_with_types = get_unit_types_for_order_list(order_list, unit1, unit2)
         if unit1.name == unit2.name:
             logging.info(order_list)
-        order_with_types = get_unit_types_for_order_list(order_list, unit1, unit2)
-        logging.info(order_with_types)
+        else:
+            logging.info(order_with_types)
