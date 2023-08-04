445e845a5f4794a5201bde9cc14c910447efaf52
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-08-04 21:35:12 +0300

    Update attack_properties.py

--- new_code/melee_fight/common/attack_properties.py
+++ new_code/melee_fight/common/attack_properties.py
@@ -8,7 +8,7 @@ def get_attack_properties(unit):
 
     min_damage = unit.min_damage
     max_damage = unit.max_damage
-    multiplier = unit.wheel_of_fate()
+    multiplier = unit.wheel_of_fate() * unit.quantity
     damage = randint(ceil(min_damage*multiplier), ceil(max_damage*multiplier))
 
     return attack, damage
