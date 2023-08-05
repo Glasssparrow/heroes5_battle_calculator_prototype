57cad7bb5524e401d9575b49c4bbadb1665965ba
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-08-05 16:36:34 +0300

    week in melee

--- new_code/melee_fight/common/attack_properties.py
+++ new_code/melee_fight/common/attack_properties.py
@@ -2,12 +2,18 @@ from random import randint
 from math import ceil
 
 
-def get_attack_properties(unit):
+def get_attack_properties(unit, fight_type):
 
     attack = unit.attack
-
+    if fight_type == "melee":
+        if unit.week_in_melee:
+            range_modifier = 0.5
+        else:
+            range_modifier = 1
+    else:
+        range_modifier = 1
     luck_modifier = unit.wheel_of_fate()
-    multiplier = luck_modifier * unit.quantity
+    multiplier = luck_modifier * unit.quantity * range_modifier
     min_damage = ceil(unit.min_damage * multiplier)
     max_damage = ceil(unit.max_damage * multiplier)
     damage = randint(min_damage, max_damage)
--- new_code/melee_fight/melee.py
+++ new_code/melee_fight/melee.py
@@ -1,8 +1,9 @@
 from .common.attack_properties import get_attack_properties
+from math import copysign
 
 
 def strike(attacker, defender):
-    attack, damage = get_attack_properties(attacker)
+    attack, damage = get_attack_properties(attacker, "melee")
     defender.take_damage(attack, damage)
 
 
@@ -11,3 +12,15 @@ def melee_fight(attacker, defender):
     strike(attacker, defender)
 
     strike(defender, attacker)
+
+
+def base_chance(attacker, defender):
+    chance = (
+            0.25 + copysign(1, attacker.hp - defender.hp) * 0.03 *
+            (attacker.hp / defender.hp)
+    )
+    if chance < 0.05:
+        chance = 0.05
+    elif chance > 0.75:
+        chance = 0.75
+    return chance
