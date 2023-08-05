8f9dfd345259c8618056daaef249371d5e0b72e0
Glass <125502088+Glasssparrow@users.noreply.github.com>
2023-08-05 20:56:38 +0300

    minor refactoring

---
+++ new_code/melee_fight/effects/apply.py
@@ -0,0 +1,31 @@
+from math import copysign
+from new_code.move.distance import distance_between_units
+
+
+def base_chance(attacker, defender):
+    chance = (
+            0.25 + copysign(1, attacker.hp - defender.hp) * 0.03 *
+            (attacker.hp / defender.hp)
+    )
+    if chance < 0.05:
+        chance = 0.05
+    elif chance > 0.75:
+        chance = 0.75
+    return chance
+
+
+def apply_effects_before_attack(attacker, target):
+    chance = base_chance(attacker, target)
+    for skill in attacker.skills:
+        if skill.apply_before_attack:
+            if skill.did_worked(
+                chance, distance_between_units(attacker, target)
+            ):
+                enemy_have_immune = False
+                for immune in skill.effect.immune_to_check():
+                    if target.__dict__[immune]:
+                        enemy_have_immune = True
+                if skill.target == "self":
+                    attacker.apply_effect(skill.effect)
+                else:
+                    target.apply_effect(skill.effect)
--- new_code/melee_fight/melee.py
+++ new_code/melee_fight/melee.py
@@ -1,6 +1,5 @@
 from .common.attack_properties import get_attack_properties
-from math import copysign
-from new_code.move.distance import distance_between_units
+from .effects.apply import apply_effects_before_attack
 
 
 def strike(attacker, defender):
@@ -16,23 +15,6 @@ def can_counter(attacker, target):
     return can_attack
 
 
-def apply_effects_before_attack(attacker, target):
-    chance = base_chance(attacker, target)
-    for skill in attacker.skills:
-        if skill.apply_before_attack:
-            if skill.did_worked(
-                chance, distance_between_units(attacker, target)
-            ):
-                enemy_have_immune = False
-                for immune in skill.effect.immune_to_check():
-                    if target.__dict__[immune]:
-                        enemy_have_immune = True
-                if skill.target == "self":
-                    attacker.apply_effect(skill.effect)
-                else:
-                    target.apply_effect(skill.effect)
-
-
 def melee_fight(attacker, defender):
 
     apply_effects_before_attack(attacker, defender)
@@ -41,15 +23,3 @@ def melee_fight(attacker, defender):
 
     if can_counter(attacker=defender, target=attacker):
         strike(defender, attacker)
-
-
-def base_chance(attacker, defender):
-    chance = (
-            0.25 + copysign(1, attacker.hp - defender.hp) * 0.03 *
-            (attacker.hp / defender.hp)
-    )
-    if chance < 0.05:
-        chance = 0.05
-    elif chance > 0.75:
-        chance = 0.75
-    return chance
